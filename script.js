9a963bcb3cf22163de40781acedceff197331291
SakeRamen <53336557+SakeRamen@users.noreply.github.com>
2019-08-05 13:04:06 +0300

    Math

--- script.js
+++ script.js
@@ -1,17 +1,9 @@
 console.log('begin');
 
-console.log('\" \n Hi');
-let string = ' The quick brown fox quickly jupms over lazy dog. '
-console.log(string[0]+ string[5]);
-console.log(string.indexOf('fox'));
-console.log(string.indexOf('o'));
-console.log(string.lastIndexOf('o'));
-console.log(string.replace('quick','slow'));
-let splited = string.split(' ');
-console.log(string.split(' '));
-console.log(string.split('o'));
-console.log(splited.join('-'));
-console.log(string.substr(16,3));
-console.log(string.toLowerCase());
-console.log(string.toUpperCase());
-console.log(string.trim());
+Math.pow(2,-3);
+Math.sqrt(9);
+Math.ceil(1.25);
+Math.round(1.25);
+Math.sign(42)
+Math.max(3,4,2,5);
+Math.min(1,-9,4,8);
