115a5dbc22e8d4f5d516283486ded4f77273d8f3
SakeRamen <53336557+SakeRamen@users.noreply.github.com>
2019-08-05 13:29:12 +0300

    array2

--- script.js
+++ script.js
@@ -7,3 +7,22 @@ let b = arr.map(function(x) {
 });
 
 console.log(b);
+
+b = arr.filter(function(x) {
+	return x%2==0;
+});
+
+console.log(b);
+
+let c = [] 
+arr.forEach(function(x) {
+	c.unshift(x);
+});
+
+console.log(c);
+
+d = [-1,6,3,9,0,-5,-3,2];
+console.log(d.reverse());
+console.log(d.sort());
+
+
