aa84ede1440d811fd3091f823951c90d37ef5cae
SakeRamen <53336557+SakeRamen@users.noreply.github.com>
2019-08-05 12:57:45 +0300

    work with strings

--- script.js
+++ script.js
@@ -1,15 +1,17 @@
-console.log("begin");
+console.log('begin');
 
-let d = {key: "value"};
-let a = {key: "value"};
-if(a==d){
-	console.log("True");
-}else{
-	console.log("False");
-}
-a = d;
-if(a==d){
-	console.log("True");
-}else{
-	console.log("False");
-}
+console.log('\" \n Hi');
+let string = ' The quick brown fox quickly jupms over lazy dog. '
+console.log(string[0]+ string[5]);
+console.log(string.indexOf('fox'));
+console.log(string.indexOf('o'));
+console.log(string.lastIndexOf('o'));
+console.log(string.replace('quick','slow'));
+let splited = string.split(' ');
+console.log(string.split(' '));
+console.log(string.split('o'));
+console.log(splited.join('-'));
+console.log(string.substr(16,3));
+console.log(string.toLowerCase());
+console.log(string.toUpperCase());
+console.log(string.trim());
