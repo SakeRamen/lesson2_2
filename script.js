980abfe61d393ce45a7486468fb5c5f9df7c4d6f
SakeRamen <53336557+SakeRamen@users.noreply.github.com>
2019-08-05 13:07:25 +0300

    corrected math

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 console.log('begin');
 
-Math.pow(2,-3);
-Math.sqrt(9);
-Math.ceil(1.25);
-Math.round(1.25);
-Math.sign(42)
-Math.max(3,4,2,5);
-Math.min(1,-9,4,8);
+console.log(Math.pow(2,-3));
+console.log(Math.sqrt(9));
+console.log(Math.ceil(1.25));
+console.log(Math.round(1.25));
+console.log(Math.sign(42));
+console.log(Math.max(3,4,2,5));
+console.log(Math.min(1,-9,4,8));
