1d3fed5db3113789f90739caed9b5d5841e63864
SakeRamen <53336557+SakeRamen@users.noreply.github.com>
2019-08-05 13:19:37 +0300

    set of numbers

--- script.js
+++ script.js
@@ -1,9 +1,18 @@
 console.log('begin');
 
-console.log(Math.pow(2,-3));
-console.log(Math.sqrt(9));
-console.log(Math.ceil(1.25));
-console.log(Math.round(1.25));
-console.log(Math.sign(42));
-console.log(Math.max(3,4,2,5));
-console.log(Math.min(1,-9,4,8));
+let nums = new Set();
+console.log(nums);
+nums.add(42);
+console.log(nums);
+nums.add(43);
+console.log(nums);
+console.log(nums.size);
+console.log(nums.has(3));
+console.log(nums.has(43));
+nums.delete(42);
+console.log(nums);
+for (let item of nums) {
+	console.log('nums:'+ item);
+}
+nums.clear();
+console.log(nums);
