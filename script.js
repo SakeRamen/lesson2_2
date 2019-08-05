5c988c54b8704c1ae255d6f60a3b4bf9805e7db7
SakeRamen <53336557+SakeRamen@users.noreply.github.com>
2019-08-05 13:22:54 +0300

    array of numbers

--- script.js
+++ script.js
@@ -1,18 +1,9 @@
 console.log('begin');
 
-let nums = new Set();
-console.log(nums);
-nums.add(42);
-console.log(nums);
-nums.add(43);
-console.log(nums);
-console.log(nums.size);
-console.log(nums.has(3));
-console.log(nums.has(43));
-nums.delete(42);
-console.log(nums);
-for (let item of nums) {
-	console.log('nums:'+ item);
-}
-nums.clear();
-console.log(nums);
+let arr = [1,2,3,4,5];
+
+let b = arr.map(function(x) {
+	return x*x
+});
+
+console.log(b);
